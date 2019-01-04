eab4d79e4143844a1d06d9b358ebca417047ffd1
JennicaStiehl <jennica.stiehl@gmail.com>
2019-01-04 15:57:26 -0700

    adds keys generator

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,7 +1,20 @@
 class Encrypt
-  attr_reader   :alphabet
+  attr_reader   :alphabet,
+                :keys
 
   def initialize
     @alphabet = ('a'..'z').to_a << ' '
+    @keys = get_keys
   end
+
+  def get_keys
+    rand = rand(99999)
+    @keys = pad(5, rand)
+  end
+
+  def pad(target_size, input)
+    gap = target_size - input.to_s.length
+    "0" * gap + "#{input}"
+  end
+
 end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -10,9 +10,21 @@ class EncryptTest < Minitest::Test
     assert_instance_of Encrypt, encrypt
   end
 
-  def test_it_has_attributes
+  def test_it_has_an_alphabet
     encrypt = Encrypt.new
 
     assert_equal 'a', encrypt.alphabet.first
+  end
+
+  def test_it_has_keys
+    encrypt = Encrypt.new
+
+    assert_equal 5, encrypt.keys.length
+  end
+
+  def test_it_can_pad_an_input
+    encrypt = Encrypt.new
+
+    assert_equal "00089", encrypt.pad(5,89)
   end
 end
